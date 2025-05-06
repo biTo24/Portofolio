c8b65b2a5ddc15c4c0c9d03dd2e5ded21d988a16
Arbi Pacolli <arbipacolli08@gmail.com>
2025-05-06 21:19:40 +0200

    Fixed GitHub and added somethings

--- script.js
+++ script.js
@@ -23,20 +23,3 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     });
   
-// Back to Top Button
-const backToTop = document.getElementById("backToTop");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 300) {
-    backToTop.classList.add("show");
-  } else {
-    backToTop.classList.remove("show");
-  }
-});
-
-backToTop.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  });
-});
